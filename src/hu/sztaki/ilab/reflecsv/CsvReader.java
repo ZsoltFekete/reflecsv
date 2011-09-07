d986e6ef425716103b86433a4207871073e471bf
zsfekete <zsfekete@info.ilab.sztaki.hu>
2011-09-07 02:22:20 +0200

    CsvReader first version ready

---
+++ src/hu/sztaki/ilab/reflecsv/CsvReader.java
@@ -0,0 +1,159 @@
+package hu.sztaki.ilab.reflecsv;
+
+import hu.sztaki.ilab.reflecsv.util.Split;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvReader {
+
+  private Reader reader;
+  private char separator;
+
+  private List<Object> registeredObjects = new ArrayList<Object>();
+  private List<ObjectDescriptor> objectDesciptors =
+    new ArrayList<ObjectDescriptor>();
+
+  private static class FieldDescriptor {
+    public Field field;
+    public int index;
+  }
+
+  private static class ObjectDescriptor {
+    public List<FieldDescriptor> fields = new ArrayList<FieldDescriptor>();
+  }
+
+  public CsvReader(Reader reader, char separator) {
+    this.reader = reader;
+    this.separator = separator;
+  }
+
+  public Object registerClass(Class<?> cls) {
+    Object[] emtpyList = new Object[0];
+    try {
+      Constructor constructor = cls.getDeclaredConstructor();
+      constructor.setAccessible(true);
+      Object obj = constructor.newInstance();
+      registerObject(obj);
+      return obj;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  public void registerObject(Object obj) {
+    registeredObjects.add(obj);
+  }
+
+  public <T> T registerClass() {
+/*
+    Class cls = T.class;
+    Class[] emtpyList = new Class[0];
+    Constructor constructor = cls.getConstructor(emtpyList);
+    */
+    return null;
+  }
+    
+  private ArrayList<String> header;
+  private BufferedReader bufferedreader;;
+
+  public void start() {
+    String line = null;
+    try {
+      bufferedreader = new BufferedReader(reader);
+      line = bufferedreader.readLine();
+    } catch (java.io.IOException e) {
+      e.printStackTrace();
+    }
+    // ArrayList<String> header = Split.split(line, separator);
+    header = Split.split(line, separator);
+    for (Object registeredObject : registeredObjects) {
+      ObjectDescriptor objectDescriptor = new ObjectDescriptor();
+      Class cls = registeredObject.getClass();
+      Field fieldlist[] = cls.getDeclaredFields();
+      for (int i = 0; i < fieldlist.length; i++) {
+        Field field = fieldlist[i];
+        int index = findInHeader(field.getName());
+        if (-1 == index) {
+          throw new RuntimeException("qwe");
+        }
+        FieldDescriptor fieldDescriptor = new FieldDescriptor();
+        fieldDescriptor.field = field;
+        fieldDescriptor.index = index;
+        objectDescriptor.fields.add(fieldDescriptor);
+      }
+      objectDesciptors.add(objectDescriptor);
+    }
+  }
+
+  private int findInHeader(String name) {
+    for (int i = 0; i < header.size(); ++i) {
+      if (header.get(i).equals(name)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+    
+  String line = null;
+
+  public boolean hasNext() {
+    try {
+      line = bufferedreader.readLine();
+      if (null != line) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (java.io.IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+  public void next() {
+    ArrayList<String> splittedLine = Split.split(line, separator);
+    for (int i = 0; i < registeredObjects.size(); ++i) {
+      Object obj = registeredObjects.get(i);
+      ObjectDescriptor objectDescriptor = objectDesciptors.get(i);
+      for (FieldDescriptor fieldDescriptor : objectDescriptor.fields) {
+        int index = fieldDescriptor.index;
+        Field field = fieldDescriptor.field;
+        fillField(obj, field, splittedLine.get(index));
+      }
+    } 
+  }
+
+  private void fillField(Object obj, Field field, String value) {
+    if (field.getType().equals(Integer.TYPE)) {
+      int intValue = Integer.valueOf(value);
+      try {
+        field.setInt(obj, intValue);
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      }
+    }
+
+    if (field.getType().equals(Double.TYPE)) {
+      double doubleValue = Double.valueOf(value);
+      try {
+        field.setDouble(obj, doubleValue);
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      }
+    }
+
+    if (field.getType().equals(String.class)) {
+      try {
+        field.set(obj, value);
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
--- test/hu/sztaki/ilab/reflecsv/CsvReaderTest.java
+++ test/hu/sztaki/ilab/reflecsv/CsvReaderTest.java
@@ -5,6 +5,8 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import java.util.ArrayList;
+import java.io.Reader;
+import java.io.StringReader;
 
 public class CsvReaderTest extends TestCase {
 
@@ -28,6 +30,83 @@ public class CsvReaderTest extends TestCase {
     } catch (NullPointerException e) {}
   }
 
+  public void testConstructor() {
+    Reader stringReader = new StringReader("");
+    CsvReader csvReader = new CsvReader(stringReader, ',');
+  }
+
+  private static class Record1 {
+    public int field1;
+    public String field2;
+  }
+
+  private static class Record2 {
+    public String field2;
+    public double field3;
+  }
+
+  public void testRegisterClass() {
+    Reader stringReader = new StringReader("");
+    CsvReader csvReader = new CsvReader(stringReader, ',');
+    Record1 record1 = (Record1) csvReader.registerClass(Record1.class);
+    Record2 record2 = (Record2) csvReader.registerClass(Record2.class);
+//    Record1 record1 = csvReader.registerClass();
+    assertNotNull(record1);
+    assertTrue(record1 instanceof Record1);
+    assertNotNull(record2);
+    assertTrue(record2 instanceof Record2);
+  }
+
+  public void testRun() {
+    String inputString =
+      "field1,field2,field3\n" +
+      "1,qwe,3.4\n" +
+      "-3,asd,-4.5\n";
+    Reader stringReader = new StringReader(inputString);
+    CsvReader csvReader = new CsvReader(stringReader, ',');
+    Record1 record1 = (Record1) csvReader.registerClass(Record1.class);
+    Record2 record2 = new Record2();
+    csvReader.registerObject(record2);
+    csvReader.start();
+    assertTrue(csvReader.hasNext());
+    csvReader.next();
+    assertEquals(1, record1.field1);
+    assertEquals("qwe", record1.field2);
+    assertEquals("qwe", record2.field2);
+    assertEquals(3.4, record2.field3);
+    assertTrue(csvReader.hasNext());
+    csvReader.next();
+    assertEquals(-3, record1.field1);
+    assertEquals("asd", record1.field2);
+    assertEquals("asd", record2.field2);
+    assertEquals(-4.5, record2.field3);
+    assertFalse(csvReader.hasNext());
+  }
+
+  public void testWhileLoop() {
+    String inputString =
+      "field1,field2,field3\n" +
+      "1,qwe,3.4\n" +
+      "-3,asd,-4.5\n";
+    Reader stringReader = new StringReader(inputString);
+    CsvReader csvReader = new CsvReader(stringReader, ',');
+    Record1 record1 = (Record1) csvReader.registerClass(Record1.class);
+
+    int counter = 0;
+
+    csvReader.start();
+    while (csvReader.hasNext()) {
+      csvReader.next();
+      if (0 == counter) {
+        assertEquals(1, record1.field1);
+      }
+      if (1 == counter) {
+        assertEquals(-3, record1.field1);
+      }
+      ++counter;
+    }
+    assertEquals(2, counter);
+  }
 
   public static Test suite() {
     return new TestSuite(CsvReaderTest.class);
