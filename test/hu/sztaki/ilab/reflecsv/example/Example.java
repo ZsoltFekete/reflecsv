6cef5e8411214a2134f38778d1fba164bb141868
zsfekete <zsfekete@ilab.sztaki.hu>
2011-09-07 10:49:22 +0200

    add test/hu/sztaki/ilab/reflecsv/example/Example.java

---
+++ test/hu/sztaki/ilab/reflecsv/example/Example.java
@@ -0,0 +1,34 @@
+package  hu.sztaki.ilab.reflecsv.example;
+
+import hu.sztaki.ilab.reflecsv.CsvReader;
+
+public class Example {
+
+  private static class Record {
+    public int field1;
+    public String field2;
+  }
+
+  public static void main(String args[]) {
+    String fileName = args[0];
+
+    java.io.Reader fileReader = null;
+    try {
+      fileReader = new java.io.FileReader(args[0]);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    CsvReader csvReader = new CsvReader(fileReader, ',');
+
+    Record record = (Record) csvReader.registerClass(Record.class);
+
+    csvReader.start();
+    while (csvReader.hasNext()) {
+      csvReader.next();
+      System.out.println("field1 = " + record.field1);
+      System.out.println("field2 = " + record.field2);
+    }
+  }
+}
