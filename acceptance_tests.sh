6c18a851b5e2965d56ba2b787241368328c6ab9a
zsfekete <zsfekete@ilab.sztaki.hu>
2011-09-08 15:27:56 +0200

    RecordMapReaderByFileNameExample.java

--- acceptance_tests.sh
+++ acceptance_tests.sh
@@ -17,6 +17,11 @@ java -cp build hu.sztaki.ilab.reflecsv.example.RecordReaderByFileNameExample tes
 diff test_data/expected_test_result.txt "$TEST_OUT"
 rm "$TEST_OUT"
 
+TEST_OUT=test_data/test_out.txt
+java -cp build hu.sztaki.ilab.reflecsv.example.RecordMapReaderByFileNameExample test_data/test_file.csv 1>"$TEST_OUT"
+diff test_data/expected_test_result2.txt "$TEST_OUT"
+rm "$TEST_OUT"
+
 echo Finished
 
 exit
---
+++ test/hu/sztaki/ilab/reflecsv/example/RecordMapReaderByFileNameExample.java
@@ -0,0 +1,46 @@
+package  hu.sztaki.ilab.reflecsv.example;
+
+import hu.sztaki.ilab.reflecsv.RecordMapReader;
+import hu.sztaki.ilab.reflecsv.IdRecord;
+import java.util.Map;
+
+public class RecordMapReaderByFileNameExample {
+
+  private static class Record {
+    public double field3;
+    public String field2;
+    public Record clone() {
+      Record copy = new Record();
+      copy.field2 = field2;
+      copy.field3 = field3;
+      return copy;
+    }
+  }
+
+  private static class Id implements IdRecord<Integer> {
+    public Integer field1;
+    public Integer getId() {
+      return field1;
+    }
+  }
+
+  public static void main(String args[]) {
+    String fileName = args[0];
+
+    RecordMapReader<Integer, Id, Record> recordReader = null;
+    try {
+      recordReader = new RecordMapReader<Integer, Id, Record>(fileName,
+          ',', new Id(), new Record());
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  
+    Map<Integer, Record> records = recordReader.read();
+    System.out.println("size=" + records.size());
+    System.out.println("1->" + records.get(1).field2 + "," +
+        records.get(1).field3);
+    System.out.println("-3->" + records.get(-3).field2 + "," +
+        records.get(-3).field3);
+  }
+}
